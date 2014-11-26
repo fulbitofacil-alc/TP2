5da2b0610eb9a6bc6be0b16538ac38f14edcf8db
williamluna <william.luna@cignium.com>
2014-11-26 15:40:55 -0500

    First Commit

---
+++ TP2_PacificoSeguros/PS.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PS.Web.Startup))]
+namespace PS.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
